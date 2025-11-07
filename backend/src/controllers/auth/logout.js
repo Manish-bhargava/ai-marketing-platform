670163371629557428957a88af81cf5a9907583d
manish bhargava <manishbhargava@manishs-MacBook-Air.local>
2025-11-07 23:46:04 +0530

    Move backend code to backend/src and remove old src folder

--- backend/src/controllers/auth/logout.js
+++
@@ -1,7 +0,0 @@
-function logout(req, res) {
-  // Assuming you are using JWT stored in cookies for authentication
-  res.clearCookie('token'); // Clear the token cookie
-  return res.status(200).json({ message: 'user Logged out successfully' });
-}
-
-module.exports = logout;
